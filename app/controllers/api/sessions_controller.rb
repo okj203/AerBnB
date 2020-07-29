75d975e3d1e0c30043a3dca54390195d059de00c
okj203 <okj203@gmail.com>
2020-07-29 09:58:58 -0400

    AerBnB

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -2,6 +2,7 @@ class Api::SessionsController < ApplicationController
     # before_action :require_logged_in, only: [:destroy]
     skip_before_action :verify_authenticity_token
     def create
+        # debugger
         @user = User.find_by_credentials(
             params[:user][:username],
             params[:user][:password]
@@ -11,10 +12,11 @@ class Api::SessionsController < ApplicationController
             login!(@user)
             render "api/users/show"
         else
+            # debugger
             # flash.now[:errors] = ['Invalid username or password.']
             # render json: { error: "invalid credentials" }
             # render json: { errors: @user.errors }
-            render json: @user.errors.full_messages, status: 404
+            render json: ["Invalid credentials."], status: 404
         end
     end
 
@@ -24,7 +26,10 @@ class Api::SessionsController < ApplicationController
             logout!
             render "api/users/show"
         else
-            render json: @user.errors.full_messages, status: 404
+            render json: ["No user logged in."], status: 404
+            # b/c I don't have a user class (nill class) to call errors messages on 
+            # 
+
             # render json: {
             #         errors: "no user logged in",
             #         status: 404
