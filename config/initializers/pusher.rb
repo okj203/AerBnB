c8928c15cf92d7d75684d379af60f6bd92b6b8fc
okj203 <okj203@gmail.com>
2020-08-03 11:02:58 -0400

    AerBnB

---
+++ config/initializers/pusher.rb
@@ -0,0 +1,8 @@
+# Pusher.app_id = ENV["pusher_app_id"]
+# Pusher.key    = ENV["pusher_key"]
+# Pusher.secret = ENV["pusher_secret"]
+
+# To require configuration keys lazily, reference the variables via "bang" methods on Figaro.env:
+Pusher.app_id = Figaro.env.pusher_app_id!
+Pusher.key    = Figaro.env.pusher_key!
+Pusher.secret = Figaro.env.pusher_secret!
---
+++ frontend/util/route_utiil.jsx
@@ -0,0 +1,32 @@
+import { Route, withRouter, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import React from "react";
+import { openModal } from "../actions/modal_actions";
+
+const Auth = ({ component: Component, path, loggedIn, exact }) => (
+  <Route
+    path={path}
+    exact={exact}
+    render={(props) =>
+      !loggedIn ? <Component {...props} /> : <Redirect to="/" />
+    }
+  />
+);
+
+const handleClick = () => openModal("login");
+
+const Protected = ({ component: Component, path, loggedIn, exact }) => (
+  <Route
+    path={path}
+    exact={exact}
+    render={(props) => (loggedIn ? <Component {...props} /> : handleClick())}
+  />
+);
+
+const mapStateToProps = (state) => {
+  return { loggedIn: Boolean(state.session.id) };
+};
+
+export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
+
+export const ProtectedRoute = withRouter(connect(mapStateToProps)(Protected));
