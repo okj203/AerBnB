4cd2e34279a069857b90a3db4f7ff9e71a689366
okj203 <okj203@gmail.com>
2020-08-07 10:03:17 -0400

    AerBnB

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'csv'
+require 'faker'
 # require 'byebug'
 # debugger
 User.destroy_all # clear out user db
